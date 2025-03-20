a094f108d1c85423204dc3f75cee65188f22aaed
dungdeptraivc <nguyendoandung123456789@gmail.com>
2025-03-20 11:56:11 +0700

    dung

---
+++ Test/resources/views/fontend/compoment/footter.blade.php
@@ -0,0 +1,54 @@
+<footer>
+    <div class="container">
+        <div class="row dung">
+            <div class="col-4 ">
+                <div class="">
+                    <h3>iLanding</h3>
+                    <p> A108 Adam Streat </p>
+                    <p> New York NY 100122 </p>
+                    <p> Phone + 155805548855</p>
+                    <p> Gmail: dunggggmail.com </p>
+                </div>
+            </div>
+            <div class="col-2  ">
+                <div class=""> <h6>Useful Links</h6>
+                <p> Home</p>
+                <p> About us</p>
+                <p> Services</p>
+                <p> Terms of service</p>
+                <p> Privacy policy</p>
+                </div>
+            </div>
+            <div class="col-2 ">
+                <div class=""> <h6> Our Service</h6> 
+                <p>Web Design</p>
+                <p>Web Development</p>
+                <p>Product Managementn</p>
+                <p>Marketing</p>
+                <p>Graphic Design</p>
+                </div>
+            </div>
+            <div class="col-2  ">
+                <div class=""><h6> Hic solutasetp</h6>
+                <p> Lorem ipsum delor</p>
+                <p> Marketing</p>
+                <p> Directs</p>
+                <p> Lorem ipsum delor</p>
+                </div>
+            </div>
+            <div class="col-2  ">
+                <div class=""><h6> Nobls illum</h6>
+                <p> Design</p>
+                <p> Lorem ipsum delor</p>
+                <p> Other</p>
+                <p> Design</p>
+                <p> Design</p>
+                </div>
+            </div>
+        </div>
+        <div class="text-center"> 
+            <p>&copy;Copyright iLanding All Rights Reverved  </p>
+            <p>The name of the copyright owner or author of the work</p>
+        </div>
+    </div>
+</footer>
---
+++ Test/resources/views/fontend/compoment/header.blade.php
@@ -0,0 +1,39 @@
+
+    <div class="header-bg shadow-sm">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light ">
+                <a href="" class="nav-link h3">iLanding</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item"> <a href="#" class="nav-link"> Home </a></li>
+                        <li class="nav-item"> <a href="" class="nav-link"> About </a></li>
+                        <li class="nav-item"> <a href="" class="nav-link"> Feature </a></li>
+                        <li class="nav-item"> <a href="" class="nav-link"> Service </a></li>
+                        <li class="nav-item"><a href="" class="nav-link"> Pricing </a></li>
+                        <li class="nav-item dropdown">
+                            <a href="" class="nav-link dropdown-toggle" id="navbarDropdown" role=" button"
+                                data-bs-toggle="dropdown"> Dropdown</a>
+                            <ul class="dropdown-menu">
+                                <li class="dropdown-item"> Test1</li>
+                                <li class="dropdown-item"> Test2</li>
+                                <li class="dropdown-item"> Test3</li>
+                            </ul>
+                        </li>
+                        <li class="nav-item"><a href="" class="nav-link"> Contact </a></li>
+                    </ul>
+
+                    <div class="header_getstarted">
+                        <a href="" class="nav-link h6"> Get Started</a>
+                    </div>
+                </div>
+            </nav>
+
+        </div>
+
+    </div>
+    </div>
+
